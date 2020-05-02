0fc9f7a7d1db0194ce8134095adc88eab797685b
Mark Sayers <marksayers46@icloud.com>
2020-05-01 21:35:48 -0500

    Setup App

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from "axios";
+import { Container, Row, Col  } from "reactstrap";
 
 function App() {
+  const [post, setPost] =useState([]);
+
   return (
     <div className="App">
+      <Container>
+        <Row>
+          <Col><Col xs="6" sm="4">.col-6 .col-sm-4</Col></Col>
+        </Row>
+      </Container>
       
     </div>
   );
